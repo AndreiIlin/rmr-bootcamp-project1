ab0492f1649afae554b9563bde88ada25f916789
Vitaliy Lipovetskiy <vitaliy1972li@gmail.com>
2022-10-04 00:03:56 +0300

    Add user tests

---
+++ backend/src/test/java/com/truestore/backend/user/UserControllerTest.java
@@ -0,0 +1,94 @@
+package com.truestore.backend.user;
+
+import com.truestore.backend.AbstractControllerTest;
+import com.truestore.backend.security.JWTToken;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+
+import static com.truestore.backend.user.UserTestData.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class UserControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL = UserController.REST_URL + '/';
+    @MockBean
+    private UserService userService;
+
+    @Test
+    @WithAnonymousUser
+    void signupUser() throws Exception {
+        LoginRequest request = new LoginRequest();
+        request.setEmail(USER_MAIL);
+        request.setPassword(USER_PASSWORD);
+        JWTToken token = new JWTToken(request.getEmail(), VALID_TOKEN);
+        when(userService.signup(any(LoginRequest.class))).thenReturn(token);
+        perform(post(REST_URL + "signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(request)))
+                .andExpect(status().isCreated())
+                .andDo(print())
+                .andExpect(jsonPath("$.access_token").value(VALID_TOKEN));
+    }
+
+    @Test
+    @WithAnonymousUser
+    void signupUserInvalid() throws Exception {
+        LoginRequest request = new LoginRequest();
+        request.setEmail(null);
+        request.setPassword(null);
+        JWTToken token = new JWTToken(request.getEmail(), VALID_TOKEN);
+        when(userService.signup(any(LoginRequest.class))).thenReturn(token);
+        perform(post(REST_URL + "signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    @WithAnonymousUser
+    void signupUserInvalidBlankEmail() throws Exception {
+        LoginRequest request = new LoginRequest();
+        request.setEmail("");
+        request.setPassword("pass");
+        JWTToken token = new JWTToken(request.getEmail(), VALID_TOKEN);
+        when(userService.signup(any(LoginRequest.class))).thenReturn(token);
+        perform(post(REST_URL + "signup")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    @WithAnonymousUser
+    void loginUser() throws Exception {
+        LoginRequest request = new LoginRequest();
+        request.setEmail(USER_MAIL);
+        request.setPassword(USER_PASSWORD);
+        JWTToken token = new JWTToken(request.getEmail(), VALID_TOKEN);
+        when(userService.login(any(LoginRequest.class))).thenReturn(token);
+        perform(post(REST_URL + "login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.access_token").value(VALID_TOKEN));
+    }
+
+    @Test
+    @WithAnonymousUser
+    void getUnAuth() throws Exception {
+        perform(get(REST_URL + "me"))
+                .andExpect(status().isForbidden());
+    }
+
+}
---
+++ backend/src/test/java/com/truestore/backend/user/UserTestData.java
@@ -0,0 +1,9 @@
+package com.truestore.backend.user;
+
+public class UserTestData {
+
+    public static final String USER_MAIL = "admin@gmail.com";
+    public static final String USER_PASSWORD = "admin";
+    public static final String VALID_TOKEN = "valid_token";
+
+}
