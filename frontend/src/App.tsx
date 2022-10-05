a32832129b82bb616fca8401df26589c1c84ab79
Dmitriu Volchenko <Sverxsro4nik@gmail.com>
2022-10-05 19:09:16 +0500

    Creation of the structure and connection of the necessary starter libraries

---
+++ frontend/src/App.tsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/no-children-prop */
+import React, { FC } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import LoginPage from './componenst/Pages/LoginPage';
+import SignUpPage from './componenst/Pages/SignUpPage';
+
+const App: FC = () => {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/login'>Log In</Link>
+          </li>
+          <li>
+            <Link to='/signup'>Sign Up</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
