0338f48cb5ab8beb2098aab403dc1f849059b436
AndreiIlin <andrei362065@gmail.com>
2022-10-11 19:42:41 +0300

    feat: 404 page

--- frontend/src/componenst/Pages/NotFound/NotFoundPage.tsx
+++ frontend/src/componenst/Pages/NotFound/NotFoundPage.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Button, Container } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import { routes } from '../../../utils/routes';
 
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  const navigate = useNavigate();
+  const { t } = useTranslation();
+  const navigateHandle = () => {
+    navigate(routes.appPagePath());
+  };
+  return (
+    <Container fluid className="wh-100 align-items-center text-center">
+      <p className="display-1">{t('page404.header')}</p>
+      <p className="display-6">{t('page404.error')}</p>
+      <Button variant="outline-dark" onClick={navigateHandle}>{t('page404.button')}</Button>
+    </Container>
+  );
 };
 
 export default NotFoundPage;
