ae37a2ef1f479606e43497eda72c4cde5560f472
AndreiIlin <andrei362065@gmail.com>
2022-10-13 21:44:18 +0300

    feat: Add profile page draft

--- frontend/src/componenst/Pages/appPage/AppPage.tsx
+++ frontend/src/componenst/Pages/appPage/AppPage.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const AppPage = () => {
+  const {id} = useParams()
+  console.log(id);
   return <div>AppPage</div>;
 };
 
