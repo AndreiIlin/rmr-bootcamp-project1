2012dd3895985b608b81c0806d9e8bb65be32406
Sergey Simenshteyn <simenshteyn@gmail.com>
2022-10-13 23:49:45 +0700

    Add basic Contract controller tests
    Took 43 minutes

--- backend/src/test/java/com/truestore/backend/contract/ContractTestData.java
+++ backend/src/test/java/com/truestore/backend/contract/ContractTestData.java
@@ -1,5 +1,7 @@
 package com.truestore.backend.contract;
 
+import com.truestore.backend.contract.dto.CreateContractDto;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -9,4 +11,5 @@ import static com.truestore.backend.user.UserTestData.USER_1;
 public class ContractTestData {
     public static final String CONTRACT_UUID = String.valueOf(UUID.randomUUID());
     public static final Contract CONTRACT_1 = new Contract(CONTRACT_UUID, APP_1, USER_1, LocalDateTime.now());
+    public static final CreateContractDto CREATE_CONTRACT_DTO = new CreateContractDto(UUID.randomUUID());
 }
